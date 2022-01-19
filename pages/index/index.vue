1e151cfc4e89949e28cbebe616a8a99a70194453
anOTAKU <929768022@qq.com>
2022-01-19 18:27:26 +0800

    first-uni

---
+++ pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <view>
+			index
+		</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
